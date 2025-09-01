6a72021c7850c79f655df7ab8d8092beb0273636
Mercesz <ezequielmerces36@gmail.com>
2025-09-01 16:22:56 -0300

    Adicionando solução ao exercicio 1

---
+++ javaScript/ex_1.js
@@ -0,0 +1,15 @@
+let nome = "Ezequiel Merces"
+let letras = nome[0] + nome[9]
+
+console.log(letras)
+
+/*
+    Javascript 
+    JS
+*/
+
+let linguagem = "JavaScript"
+let soletrando = linguagem[0] + linguagem[4]
+console.log(soletrando)
+
+
