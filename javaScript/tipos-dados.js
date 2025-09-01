6e589279dca7c08385869e2c59acf39d4ba62ae6
Mercesz <ezequielmerces36@gmail.com>
2025-09-01 16:07:15 -0300

    adicionando novo arquivo 'tipos-dados'

---
+++ javaScript/tipos-dados.js
@@ -0,0 +1,19 @@
+//String
+let nome = "Ezequiel"
+
+//Inteiros
+let idade = 21
+
+//Booleanos
+let casado = true
+
+// Números flutuantes com casa decimais
+let valorProduto = 5.35
+
+//Resultado
+console.log("Meu nome é " + nome)
+console.log("Minha idade é: ", idade)
+console.log("Casado? ", casado)
+console.log("R$",valorProduto)
+console.log(typeof idade)
+console.log(typeof nome)
