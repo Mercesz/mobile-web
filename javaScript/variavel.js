66c028ab48504794651369fa61abdd2711be5e00
Mercesz <ezequielmerces36@gmail.com>
2025-09-01 15:54:22 -0300

    Adicionando arquivo variavel no repositorio de JS

---
+++ javaScript/variavel.js
@@ -0,0 +1,11 @@
+let mensagem = "Olá, mundo!"
+
+const nomeCompleto = "Ezequiel Merces"
+
+let idade = 21
+
+// Resultado
+
+console.log(mensagem)
+console.log(nomeCompleto)
+console.log("Meu nome é " + nomeCompleto + " e minha idade é: " + idade + " anos")
