a5da66b006bc94f1ee1978fe09daf32109b04303
Mercesz <ezequielmerces36@gmail.com>
2025-09-02 14:49:57 -0300

    \Resolvendo o exercicio 2 e adicionando novo arquivo calc

---
+++ javaScript/calc.js
@@ -0,0 +1,23 @@
+// Duas variáveis
+let num1 = 300
+let num2 = 10
+
+// Operações
+
+//Soma
+let soma = num1 + num2
+
+//Subtração
+let sub = num1 - num2
+
+//Multiplicação
+let mult = num1 * num2
+
+//Divisão
+let div = num1 / num2
+
+//Imprimir as respostas
+console.log("O valor da soma é: " + soma)
+console.log("O valor da subtração é: " + sub)
+console.log("O valor da multiplicação é: " + mult)
+console.log("O valor da divisão é: " + div)
---
+++ javaScript/ex_2.js
@@ -0,0 +1,9 @@
+/*
+    - Exiba no console
+        A quantidade de caracteres que a const possui
+*/
+
+const linguagem = "JavaScript";
+
+console.log(linguagem.length)
+
