3c7cda335dcd957362b9d96473b32b9a6e4c4756
Mercesz <ezequielmerces36@gmail.com>
2025-09-02 16:19:09 -0300

    Adicionando algumas funções no html

--- javaScript/calc.js
+++ javaScript/calc.js
@@ -1,24 +1,26 @@
 // Duas variáveis
-let num1 = Number(prompt("Digite um número: "))
-let num2 = Number(prompt("Digite outro: "))
+let num1 = 10
+let num2 = 10
 
 // Operações
-
-//Soma
 let soma = num1 + num2
-
-//Subtração
 let sub = num1 - num2
-
-//Multiplicação
 let mult = num1 * num2
-
-//Divisão
 let div = num1 / num2
 
-//Imprimir as respostas
-console.log("O valor da soma é: ", soma)
-console.log("O valor da subtração é: ", sub)
-console.log(`O valor da multiplicação é: ${mult}`)
-console.log("O valor da divisão é: ", div)
+//Funções
+function somar() {
+    console.log(soma)
+}
 
+function subtracao() {
+    console.log(sub)
+}
+
+function multiplicacao() {
+    console.log(mult)
+}
+
+function divisao() {
+    console.log(div)
+}
