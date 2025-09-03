56356f965e31817959bf5337ada4ff356c6a374b
Mercesz <ezequielmerces36@gmail.com>
2025-09-03 16:14:34 -0300

    Adicionando funções a nova calc

---
+++ javaScript/calcNew.js
@@ -0,0 +1,26 @@
+//Funções
+function somar(iden, num1, num2) {
+    console.log(iden, num1 + num2)
+}
+
+//sub
+function sub(iden, num1, num2) {
+    console.log(iden, num1 - num2)
+}
+
+//mult
+function mult(iden, num1, num2) {
+    console.log(iden, num1 * num2)
+}
+
+//div
+function div(iden, num1, num2) {
+    console.log(iden, num1 / num2)
+}
+
+//Imprimir funções
+somar("Soma ", 10, 10)
+sub("Sub ", 10, 20)
+mult("Mult ", 30, 10)
+div("Div", 40, 10)
+
