e185b7e921e079ce5351a6724707ed811fbe067b
Mercesz <ezequielmerces36@gmail.com>
2025-09-09 16:20:11 -0300

    Testando estrutura de repetição While e incrementando a tabuada

---
+++ javaScript/tabuada.js
@@ -0,0 +1,9 @@
+// Tabuada 7
+
+let multiplicador = 7
+let i = 1
+
+while (i <= 10) {
+    console.log(`${multiplicador} X ${i} = ${multiplicador * i}`)
+    i++
+}
---
+++ javaScript/while.js
@@ -0,0 +1,8 @@
+//While
+let i = 1
+let y = 1
+
+while (i <= 10) {
+    console.log(i)
+    i++
+}
