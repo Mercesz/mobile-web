12f55c97885f48511a4a40b478da2bf2f81ab440
Mercesz <ezequielmerces36@gmail.com>
2025-09-09 15:44:33 -0300

    Resolvendo o exercicio 1 da estrutura de repetição

---
+++ javaScript/exer1.js
@@ -0,0 +1,4 @@
+for (let i = 1; i <= 10; i++ ) {
+    // console.log("Esta é a " + i + "° vez que está frase é exibida no console")
+    console.log(`Esta é a ${i}ª vez que está frase é exibida no console`)
+}
