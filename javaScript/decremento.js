bf2eb6abe33c6493f65c42f1fd3ab1f8fc85dc8a
Mercesz <ezequielmerces36@gmail.com>
2025-09-09 14:39:11 -0300

    Adicionando arquivos Incremento e Decremento para teste

---
+++ javaScript/decremento.js
@@ -0,0 +1,25 @@
+let valor = 7
+
+valor = valor - 1
+
+console.log(valor) //Saída 6
+
+valor--
+
+console.log(valor) //Saída 5
+
+valor -= 1
+
+console.log(valor)
+
+// Multiplicação
+
+valor *= 2;
+
+console.log(valor)
+
+// Divisão
+valor /= 2
+console.log(valor)
+
+
---
+++ javaScript/incremento.js
@@ -0,0 +1,15 @@
+let valor = 7
+
+console.log(valor) // Saída 7 
+
+valor = valor+1
+console.log(valor) //Saída 8
+
+valor++
+valor++
+
+console.log(valor) //Saída 10
+
+valor += 3
+
+console.log(valor) //Saída 13
