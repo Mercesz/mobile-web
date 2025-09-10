2be81691bf5ce44e062c30a206c8af8a17c3eefa
Mercesz <ezequielmerces36@gmail.com>
2025-09-10 16:09:31 -0300

    Adicionando estilização a página nota e lógica

--- javaScript/nota.js
+++ javaScript/nota.js
@@ -1,10 +1,21 @@
 // Variáveis
-const inpuNota = document.getElementById("nota")
+const inputNota = document.getElementById("nota")
 const resultado = document.getElementById("result")
 
 // Funções
-function resultadoNotaClick() {
-    alert("Resultado")
+function resultadoNotaClick(event) {
+    event.preventDefault()
+    console.log(inputNota.value)
+
+    //Lógica - Condição
+    if (inputNota.value >= 6) {
+        resultado.innerHTML = "Aprovado!"
+    } else {
+        resultado.innerHTML = "Reprovado!"
+    }
+
+    inputNota.value = ""
+
 }
 
 
