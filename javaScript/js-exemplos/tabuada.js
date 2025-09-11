8896b16fec602f2ddf5d22a2707f021f4f9eb27d
Mercesz <ezequielmerces36@gmail.com>
2025-09-11 14:43:08 -0300

    Adiconando validação no formulário nota

--- javaScript/js-exemplos/tabuada.js
+++ javaScript/js-exemplos/tabuada.js
@@ -1,9 +1,15 @@
 // Tabuada 7
 
-let multiplicador = 7
-let i = 1
+let multiplicador = 1
 
-while (i <= 10) {
-    console.log(`${multiplicador} X ${i} = ${multiplicador * i}`)
-    i++
+
+while (multiplicador <= 10) {
+    let i = 1
+    while (i <= 10) {
+        console.log(`${multiplicador} X ${i} = ${multiplicador * i}`)
+        i++
+
+    }
+    console.log(" ")
+    multiplicador++
 }
--- javaScript/nota.js
+++ javaScript/nota.js
@@ -1,11 +1,19 @@
 // Variáveis
 const inputNota = document.getElementById("nota")
 const resultado = document.getElementById("result")
+const message = document.getElementById("message")
 
 // Funções
 function resultadoNotaClick(event) {
     event.preventDefault()
     console.log(inputNota.value)
+
+    if (inputNota.value == "") {
+        message.style.display = "block"
+        return false
+    }
+
+    message.style.display = "none"
 
     //Lógica - Condição
     if (inputNota.value >= 6) {
@@ -15,7 +23,6 @@ function resultadoNotaClick(event) {
     }
 
     inputNota.value = ""
-
 }
 
 
