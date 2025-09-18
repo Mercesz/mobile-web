976c19af2472d43bfe83eec97e0ab1d89017398d
Ezequiel Merces <ezequielmerces36@gmail.com>
2025-09-18 16:22:00 -0300

    feat:Adicionando algumas tag para teste

--- projeto-b2/src/App.jsx
+++ projeto-b2/src/App.jsx
@@ -5,6 +5,25 @@ function App() {
     <>
       <h1>Olá Rodriguinho</h1>
       <h2>Ezequiel</h2>
+      <hr />
+      <h3>Merces</h3>
+      <hr />
+      <h4>Da</h4>
+      <hr />
+      <h5>Rosa</h5>
+      <br />
+      <hr />
+      <h6>21</h6>
+      <span>Lorem ipsum dolor, <strong>sit amet consectetur adipisicing</strong> elit. Labore ducimus esse nemo sapiente. Velit perferendis eaque at, fugit corrupti esse laborum soluta in sapiente explicabo voluptate voluptatem deleniti nulla. Facere!</span>
+      <p>jdkjkdfsgkjklfkjsgklfdsjgkldsjfgklljkjklfsdçg</p>
+
+      <ul>
+        <li>sadjlsaldjj</li>
+        <li>dsjkahdjkas</li>
+        <li>hsfjkdahfsj</li>
+        <li>fjlsdhfjkjh</li>
+        <li>kalçsdksakl</li>
+      </ul>
 
     </>
   )
