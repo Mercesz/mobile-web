59e8e0490914e2a38df7b709eb1f5f84d5240408
Ezequiel Merces <ezequielmerces36@gmail.com>
2025-09-30 16:27:40 -0300

    Adicionando conteudo a página e estilização e criando a pag de cursos senai

--- meuapp/src/Telas/Prime.js
+++ meuapp/src/Telas/Prime.js
@@ -1,9 +1,46 @@
-import {View, StyleSheet,Text} from "react-native";
+import { View, StyleSheet, Text } from "react-native";
 
 export default function Prime() {
-  return (
-    <View>
-        <Text>B1 Dev - Popeto</Text>
-    </View>
-  )
+    return (
+        <>
+            <View style={estilos.topo}>
+                <Text style={estilos.tituloHeader}>Cursos SENAI</Text>
+            </View>
+
+            <View style={estilos.cursosContent}>
+                <Text style={estilos.texto}> - HTML Completo</Text>
+                <Text style={estilos.texto}> - Informática Básica </Text>
+                <Text style={estilos.texto}> - Desenvolvimento Java </Text>
+                <Text style={estilos.texto}> - Excel Básico</Text>
+                <Text style={estilos.texto}> - Microsoft Power BI</Text>
+            </View>
+        </>
+    )
 }
+
+const estilos = StyleSheet.create({
+    topo: {
+        width: "100%",
+        height: 150,
+        backgroundColor: "#E62727",
+    },
+    tituloHeader: {
+        whidth: "100%",
+        color: "white",
+        textAlign: "center",
+        paddingTop: 60,
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    cursosContent: {
+        paddingTop: 25,
+        width: "100%",
+        height: 200,
+        backgroundColor: "#C4E1E6",
+    },
+    texto: {
+        marginLeft: 20,
+        marginBottom: 8,
+        fontWeight: "bold",
+    }
+});
