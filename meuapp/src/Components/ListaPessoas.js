a97c01cb0bc65baa85744bd17de0a9235ab43512
Ezequiel Merces <ezequielmerces36@gmail.com>
2025-10-03 16:18:19 -0300

    Adicionando estilização ao componente Lista Pessoas

--- meuapp/src/Components/ListaPessoas.js
+++ meuapp/src/Components/ListaPessoas.js
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function ListaPessoas() {
     const pessoas = [
@@ -25,8 +25,39 @@ export default function ListaPessoas() {
     ];
 
     return (
-        <View>
-            <Text>Teste Testes</Text>
+        <View style={estilos.container}>
+            <Text style={estilos.titulo}>LIsta de Pessoas</Text>
+
+            {pessoas.map((pessoa) => (
+                <View style={estilos.card}>
+                    <Text>Nome: {pessoa.nome}  •  </Text>
+                    <Text>Idade: {pessoa.idade} Anos</Text>
+                </View>
+            ))}
+
         </View>
     )
 }
+
+
+const estilos = StyleSheet.create({
+    container: {
+        padding: 20,
+        marginTop: 40,
+    },
+    titulo: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 15,
+        textAlign: 'center',
+    },
+    card: {
+        flexDirection: "row",
+        borderWidth: 1,
+        borderRadius: 15,
+        borderColor: 'red',
+        marginBottom: 10,
+        padding: 10,
+        width: '100%',
+    }
+})
