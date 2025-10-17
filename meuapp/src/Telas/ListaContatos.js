28de66bfd94df6971b360eb24d114d303dae8a0c
Ezequiel Merces <ezequielmerces36@gmail.com>
2025-10-17 16:17:32 -0300

    feat: Adicionando estlização e conectado o BD a nossa aplicação

--- meuapp/src/Telas/ListaContatos.js
+++ meuapp/src/Telas/ListaContatos.js
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
-import axios from 'axios';
+import { useEffect, useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import axios from 'axios'
 
 export default function ListaContatos() {
     const [contatos, setContatos] = useState([]);
@@ -23,8 +23,47 @@ export default function ListaContatos() {
     }, [])
 
     return (
-        <View>
-            <Text>Lista Contatos</Text>
+        <View style={estilos.container}>
+            <Text style={estilos.titulo}>Lista de Contatos</Text>
+            {contatos.length > 0 ? (
+                contatos.map((contato, index) => (
+                    <View key={index} style={estilos.box}>
+                        <Text style={estilos.}>Nome: </Text>
+                        <Text >{contato.nome} ççç</Text>
+                        <Text>Telefone: </Text>
+                        <Text>{contato.telefone}</Text>
+                    </View>
+                ))
+            ) : (
+                <Text >Nenhum contato disponível</Text>
+            )}
         </View>
     )
 }
+
+const estilos = StyleSheet.create({
+    container: {
+        margin: 10,
+        backgroundColor: "white",
+        borderColor: "white",
+        borderWidth: 2,
+        padding: 20,
+        borderRadius: 40,
+        shadowColor: "#FF9013",
+        shadowOpacity: 10,
+        elevation: 10,
+    },
+    titulo: {
+        fontWeight: "bold",
+        textAlign: "center",
+        fontSize: 18,
+    },
+    box: {
+        borderWidth: 1,
+        borderRadius: 15,
+        padding: 20,
+        margin: 8,
+        alignItems: "center"
+    },
+
+})
