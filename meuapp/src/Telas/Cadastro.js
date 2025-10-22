ffbf1062bf8c26e51c6d486337caabda9e69d3f9
Mercesz <ezequielmerces36@gmail.com>
2025-10-22 16:15:25 -0300

    feat: adicionando função add novo contato

--- meuapp/src/Telas/Cadastro.js
+++ meuapp/src/Telas/Cadastro.js
@@ -1,21 +1,31 @@
 import { useState } from 'react'
 import { Alert, Button, Text, TextInput, View, StyleSheet } from 'react-native'
 import axios from 'axios'
+import { useNavigation } from '@react-navigation/native'
 
 export default function Cadastro() {
     const [nome, setNome] = useState('')
-    const [tel, setTel] = useState('')
+    const [telefone, setTelefone] = useState('')
+    const navigation = useNavigation()
 
     const enviarContato = async () => {
-        if (!nome || !tel) {
+        if (!nome || !telefone) {
             Alert.alert("Erro", "por favor, preencha todos os campos")
-            return false
+            return
         }
 
         const novoContato = { nome, telefone }
-        axios
-            .post(`http://localhost:3000/contatos`, novoContato)
 
+        axios.post(`http://10.0.2.2:3000/contatos`, novoContato)
+            .then(resposta => {
+                if (resposta.status === 201) {
+                    setNome('')
+                    setTelefone('')
+                    navigation.navigate('ListaContatos')
+                } else {
+                    Alert.alert("Erro", " falha ao adicionar contato")
+                }
+            })
     }
 
     return (
@@ -26,7 +36,7 @@ export default function Cadastro() {
                 placeholder='Digite o nome'
                 style={estilos.input}
                 value={nome}
-                onChange={setNome}
+                onChangeText={setNome}
             />
 
             <Text style={estilos.label}>Tel: </Text>
@@ -34,8 +44,8 @@ export default function Cadastro() {
             <TextInput
                 placeholder='Digite o telefone'
                 style={estilos.input}
-                value={tel}
-                onChange={setTel}
+                value={telefone}
+                onChangeText={setTelefone}
             />
 
             <Button title='Cadastrar' onPress={enviarContato} />
@@ -46,6 +56,16 @@ export default function Cadastro() {
 const estilos = StyleSheet.create({
     container: {
         padding: 20,
+        margin: 10,
+        backgroundColor: "white",
+        borderColor: "white",
+        borderWidth: 2,
+        padding: 20,
+        borderRadius: 40,
+        shadowColor: "#FF9013",
+        shadowOpacity: 0.9,
+        elevation: 2,
+        height: 300,
     },
     label: {
         fontSize: 18,
